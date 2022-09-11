8aad291ff066108c32967ceb7cd85bc1e25bbe7d
Josuel Santos <josuel.santos@aluno.ifsp.edu.br>
2022-09-11 18:40:17 -0300

    Add files via upload

---
+++ TP02/TP02-1/WebForm1.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="TP02_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" OnClick="Button1_Click" runat="server" Text="Gerar arquivo"></asp:Button>
+            <br />
+            <br />
+            <asp:TextBox ID="TextBox1" runat="server" Height="500" Width="250" Text="O conteúdo do arquivo aparecerá aqui."></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ TP02/TP02-1/WebForm1.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace TP02_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            XDocument xDoc = new XDocument
+            (
+            new XDeclaration("1.0", "utf - 8", "yes"),
+            new XComment("Arquivo XML criado via Asp.Net com sucesso!"),
+            new XElement("UserDetail",
+            new XElement("User",
+            new XAttribute("UID", "01"),
+            new XElement("Name", "João"),
+            new XElement("Address", "Cubatão")
+            ),
+            new XElement("User",
+            new XAttribute("UID", "02"),
+            new XElement("Name", "Maria"),
+            new XElement("Address", "Santos")
+            )
+            )
+            );
+
+            String xmlFile = Server.MapPath("~/User.xml");
+            xDoc.Save(xmlFile);
+
+            XElement xFile = XElement.Load(xmlFile);
+            TextBox1.Text = xFile.ToString();
+        }
+    }
+}
